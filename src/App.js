7663b4ddddbd7dfaaf43b684568062c54c2a80a5
Lucas Aranguren <arangurenlucas@live.com>
2021-06-05 14:19:50 -0300

    position fixed and links to github repo added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <h1>Tasks List</h1>
         <nav className="nav-bar">
           <a href="/">Home</a>
+          <a href='/folders'>View Folders</a>
         </nav>
       </div>
       <div className="routes">
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-      <>
-      <Link to='/folders'>View folders</Link>
-      </>
+    <div className="home">
+      <h4>You can see the code on the following links</h4>
+      <a
+        href="https://github.com/arangurenlucas/tasksList/tree/main/backend"
+        target="_blank"
+      >
+        Backend Github Repository
+      </a>
+      <a
+        href="https://github.com/arangurenlucas/taskList-frontend"
+        target="_blank"
+      >
+        Frontend Github Repository
+      </a>
+    </div>
   );
 }
